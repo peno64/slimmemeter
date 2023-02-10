48bdfc11484dc5fc4bde6dee3838f8a4af2d70aa
peno64 <32430988+peno64@users.noreply.github.com>
2023-02-10 23:16:29 +0100

    Add Electricity Peak Current Month

--- fix.bat
+++ fix.bat
@@ -1,2 +1,11 @@
+powershell -Command "(gc .\.esphome\build\slimmelezer\.piolibdeps\slimmelezer\Dsmr\src\dsmr\fields.h) -replace '/\* Meter Reading electricity delivered to client \(Special for Lux\) in 0,001 kWh \*/', '/* Water Consumed in m3 */' | Out-File -encoding ASCII .\.esphome\build\slimmelezer\.piolibdeps\slimmelezer\Dsmr\src\dsmr\fields.h.pat"
+move /Y .\.esphome\build\slimmelezer\.piolibdeps\slimmelezer\Dsmr\src\dsmr\fields.h.pat .\.esphome\build\slimmelezer\.piolibdeps\slimmelezer\Dsmr\src\dsmr\fields.h
+
 powershell -Command "(gc .\.esphome\build\slimmelezer\.piolibdeps\slimmelezer\Dsmr\src\dsmr\fields.h) -replace 'DEFINE_FIELD *\( *energy_delivered_lux *, *FixedValue *, *ObisId *\( *1 *, *0 *, *1 *, *8 *, *0 *\) *, *FixedField *, *units::kWh, *units::Wh *\) *;', 'DEFINE_FIELD(energy_delivered_lux, TimestampedFixedValue, ObisId(0, WATER_MBUS_ID, 24, 2, 1), TimestampedFixedField, units::m3, units::dm3);' | Out-File -encoding ASCII .\.esphome\build\slimmelezer\.piolibdeps\slimmelezer\Dsmr\src\dsmr\fields.h.pat"
+move /Y .\.esphome\build\slimmelezer\.piolibdeps\slimmelezer\Dsmr\src\dsmr\fields.h.pat .\.esphome\build\slimmelezer\.piolibdeps\slimmelezer\Dsmr\src\dsmr\fields.h
+
+powershell -Command "(gc .\.esphome\build\slimmelezer\.piolibdeps\slimmelezer\Dsmr\src\dsmr\fields.h) -replace '/\* Meter Reading electricity delivered by client \(Special for Lux\) in 0,001 kWh \*/', '/* Electricity Peak Current Month in kW */' | Out-File -encoding ASCII .\.esphome\build\slimmelezer\.piolibdeps\slimmelezer\Dsmr\src\dsmr\fields.h.pat"
+move /Y .\.esphome\build\slimmelezer\.piolibdeps\slimmelezer\Dsmr\src\dsmr\fields.h.pat .\.esphome\build\slimmelezer\.piolibdeps\slimmelezer\Dsmr\src\dsmr\fields.h
+
+powershell -Command "(gc .\.esphome\build\slimmelezer\.piolibdeps\slimmelezer\Dsmr\src\dsmr\fields.h) -replace 'DEFINE_FIELD *\( *energy_returned_lux *, *FixedValue *, *ObisId *\( *1 *, *0 *, *2 *, *8 *, *0 *\) *, *FixedField *, *units::kWh, *units::Wh *\) *;', 'DEFINE_FIELD(energy_returned_lux, TimestampedFixedValue, ObisId(1, 0, 1, 6, 0), TimestampedFixedField, units::kW, units::W);' | Out-File -encoding ASCII .\.esphome\build\slimmelezer\.piolibdeps\slimmelezer\Dsmr\src\dsmr\fields.h.pat"
 move /Y .\.esphome\build\slimmelezer\.piolibdeps\slimmelezer\Dsmr\src\dsmr\fields.h.pat .\.esphome\build\slimmelezer\.piolibdeps\slimmelezer\Dsmr\src\dsmr\fields.h
